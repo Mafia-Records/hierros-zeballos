6be01ddd4b1e03b384bdf7edceac1eff8c830530
luis salinas <luigicoder28@gmail.com>
2020-11-06 18:59:16 -0300

    zeballos 04

---
+++ js/sweetalert.js
@@ -0,0 +1,8 @@
+
+    Swal.fire ({
+        background: "white",
+        html:"<h2 class='text-orange'>Estimados Clientes:</h2><p class='mb-10'>Les comunicamos que las siguiente sucursales de Zeballitos se encuentran abiertas y estan atendiendo al público:</p><h3 class='h3'>Primera Sucursal:</h3><p>Av.??? 544 de Lunes a Viernes de 7:30 a 17hs y Sábados de 7:30 a 13hs</p><p class='mb-2'>WhatsApp: 11 67890948 | 11 67839836</p><h3 class='h3'>Segunda Sucursal:</h3><p>Av.??? 544 de Lunes a Viernes de 7:30 a 17hs y Sábados de 7:30 a 13hs</p><p class='mb-2'>WhatsApp: 11 67890948 | 11 67839836</p><h3 class='h3'>Tercera Sucursal:</h3><p>Av.??? 544 de Lunes a Viernes de 7:30 a 17hs y Sábados de 7:30 a 13hs</p><p class='mb-3'>WhatsApp: 11 67890948 | 11 67839836</p><h3 class='reparto mt-5 mb-5'>Reparto esta habilitado de forma condicional</h3><h3 class='familia'>Los saluda atentamente: La Familia de Hierros Zeballos</h3>",
+        width:"70%",
+        confirmButtonText:"Continuar"
+    })
+
