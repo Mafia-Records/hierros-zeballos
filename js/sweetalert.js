48c5d77f4dc1565e08861758e7a210faf5afaf50
Cesar <cesar.torke@gmail.com>
2020-11-10 19:51:35 -0300

    zeballos 07

---
+++ js/sweetalert.js
@@ -0,0 +1,8 @@
+
+    Swal.fire ({
+        background: "white",
+        html:"<h2 class='text-orange'>Estimados Clientes:</h2><h3 class='h3'>Les comunicamos el protocolo de prevencion de Covid que implementamos en Hierros Zeballos para atención al público:</h3><br><h3 class='h3'>Uso de barbijo:</h3><p>Obligatorio para el ingreso al local.</p><h3 class='h3'>Distanciamiento:</h3><p>Mantené 2 mts de distancia con otras personas.</p><h3 class='h3'>Uso de alcohol en gel:</h3><p>Sanitizate las manos al ingresar al local.</p></p><h3 class='reparto mt-5 mb-5'>El reparto esta habilitado de forma condicional</h3><h3 class='familia'>Los saluda atentamente: La Familia de Hierros Zeballos</h3>",
+        width:"70%",
+        confirmButtonText:"Continuar"
+    })
+
