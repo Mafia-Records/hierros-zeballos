ad3dbceaf067fabb27bff5a79329c50346275a74
luis salinas <luigicoder28@gmail.com>
2020-11-20 21:14:19 -0300

    zeballos 10

--- js/sweetalert.js
+++ js/sweetalert.js
@@ -1,8 +1,13 @@
+/*Swal.fire ({
+    background: "orange",
+    html:"<h2 class='text-black estimados'>Estimados Clientes:</h2><h3 class='h3'>Les comunicamos el protocolo de prevencion de Covid que implementamos en Hierros Zeballos para atención al público:</h3><br><h3 class='h3'>Uso de barbijo:</h3><p class='text-red'>Obligatorio para el ingreso al local.</p><h3 class='h3'>Distanciamiento:</h3><p class='text-red'>Mantené 2 mts de distancia con otras personas.</p><h3 class='h3'>Uso de alcohol en gel:</h3><p class='text-red'>Sanitizate las manos al ingresar al local.</p></p><h3 class='reparto mt-5 mb-5 h3'>El reparto esta habilitado de forma condicional</h3><h3 class='familia h3'>Los saluda atentamente: La Familia de Hierros Zeballos</h3>",
+    width:"99%",
+    confirmButtonText:"Continuar"
+})*/
+
 Swal.fire ({
-    background: "white",
-    html:"<h2 class='text-red estimados'>Estimados Clientes:</h2><h3 class='h3'>Les comunicamos el protocolo de prevencion de Covid que implementamos en Hierros Zeballos para atención al público:</h3><br><h3 class='h3'>Uso de barbijo:</h3><p class='text-red'>Obligatorio para el ingreso al local.</p><h3 class='h3'>Distanciamiento:</h3><p class='text-red'>Mantené 2 mts de distancia con otras personas.</p><h3 class='h3'>Uso de alcohol en gel:</h3><p class='text-red'>Sanitizate las manos al ingresar al local.</p></p><h3 class='reparto mt-5 mb-5 h3'>El reparto esta habilitado de forma condicional</h3><h3 class='familia h3'>Los saluda atentamente: La Familia de Hierros Zeballos</h3>",
+    background: "orange",
+    html:"<h2>Estimados Clientes:</h2><p class='mb-1'>Les comunicamos que el local Hierros Zeballos se encuentra abierto y estamos atendiendo al público:</p><h4>Casa Central:</h4><p>Av. Guillermo Hudson 216, B1888 Florencio Varela, Provincia de Buenos Aires</p><p class='mb-2'>Atencion!!! por la situacion actual del Corona Virus les informamos que nuestro horario de atencion se ve afectado, y estamos atendiendo de Lunes a Viernes de 8:00 a 15hs y Sábados de 8:00 a 13hs</p><p class='mb-5'>WhatsApp: 11-3228-3411</p><h4 class='reparto mt-5 mb-2'>Reparto esta habilitado de forma condicional</h4><h4 class='familia'>Los saluda atentamente: La Familia de Hierros Zeballos</h4>",
     width:"99%",
     confirmButtonText:"Continuar"
 })
-
-
